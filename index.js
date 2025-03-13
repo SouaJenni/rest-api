1d0e8ad4c55ecb9f0cb7edf45d624af31922a995
SouaJenni <jennifervialli1@gmail.com>
2025-03-13 17:43:58 -0300

    /saudacao

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+const express = require("express");
+const app = express();
+const porta = 3000;
+
+app.listen(porta, ()=>{
+    console.log("Servidor rodando na porta 3000");
+});
+
+app.get('/', (requisicao, resposta)=>{
+    resposta.send("Olá, mundo!");
+});
+
+app.get('/abacaxi', (requisicao, resposta)=>{
+    console.log(requisicao);
+    resposta.send(`Olá, ${requisicao.query.nome}`);
+});
+
+app.get('/saudacao', (requisicao, resposta)=>{
+    console.log(requisicao);
+    if(requisicao.query.nome === "" || requisicao.query.nome === null || requisicao.query.nome === undefined){
+        resposta.send(`Olá, estranho!`);
+    }else {
+        resposta.send(`Olá, ${requisicao.query.nome}!`);
+    }
+});
