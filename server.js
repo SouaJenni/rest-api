4382bea70c75ab722e3a7edb2aeee7996f8a094a
SouaJenni <jennifervialli1@gmail.com>
2025-04-11 16:38:21 -0300

    Adicionando todos os testes unitários utilizando jest

--- server.js
+++ server.js
@@ -12,23 +12,29 @@ const pessoas = [
 ];
 
 app.get('/pessoas', (request, response)=>{
+    console.log("Pegando pessoas");
     return response.status(200).send(pessoas);
 });
 
 app.get('/pessoas/:id', (request, response)=>{
+    console.log("Pegando pessoa ", id);
     const pessoa = pessoas.find((p) => p.id === request.params.id);
     if(pessoa){
         return response.status(200).send(pessoa);
     }
+    console.log("Pessoa não encontrada");
     return response.status(404).send({mensagem: "Pessoa não encontrada"});
 });
 
 app.post('/pessoas', (request, response) =>{
+    console.log("Criando pessoa");
     if(request.body?.nome === undefined || request.body?.idade === undefined || request.body?.id === undefined){
+        console.log("Sem dados para adicionar");
         return response.status(400).send({mensagem: "Sem dados para adicionar"});
     }
     for (let i = 0; i < pessoas.length; i++) {
         if(pessoas[i].id === request.body.id) {
+            console.log("Este usuário já existe");
             return response.status(400).send({mensagem: "Este usuário já existe"});
         }
     }
@@ -37,21 +43,25 @@ app.post('/pessoas', (request, response) =>{
 });
 
 app.put('/pessoas/:id', (request, response)=>{
+    console.log("Atualizando pessoa");
     const pessoa = pessoas.find((p) => p.id === request.params.id);
     if(pessoa){
         Object.assign(pessoa, request.body);
         return response.status(200).send(pessoa);
     }
+    console.log("Pessoa não encontrada");
     response.status(404).send({mensagem: "Pessoa não encontrada"});
 
 });
 
 app.delete('/pessoas/:id', (request, response)=>{
+    console.log("Deletando pessoa");
     let index = pessoas.findIndex(pessoa=> pessoa.id === request.params.id);
     if (index !== -1) {
         pessoas.splice(index, 1);
         return response.status(200).send(pessoas);
     }
+    console.log("Pessoa não encontrada");
     response.status(404).send({mensagem: "Pessoa não encontrada"});
 });
 
